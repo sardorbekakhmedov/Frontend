4333352e34f8decddbeb06174f1d5a380e7cb12f
Sardorbek Akhmedov <110537414+sardorbekakhmedov@users.noreply.github.com>
2024-12-25 18:20:27 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ProfileCardComponent} from './common-ui/profile-card/profile-card.component';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    ProfileCardComponent,
+    JsonPipe
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'tik-talk';
+
 }
